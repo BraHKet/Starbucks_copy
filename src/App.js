d088aa81ceec0a9ed3c408193fc4ba0f88e57f5d
Lorenzo <lore.mail.gl@gmail.com>
2024-01-29 12:30:24 +0100

    Versione v1.0.0 del sito di Starbucks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './img/starb.png';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <div className="header">
+          <img src={logo} alt=''/>
+          <div className="caffe">CAFFÈ
+            <div className="sub_caffe">
+              <ul>
+                <li id='caffe1'>Il nostro caffè</li>
+                <li id='caffe2'>Espresso</li>
+                <li id='caffe3'>Preparazione caffè</li>
+              </ul>
+            </div>
+          </div>
+          <div className="menu">MENÙ</div>
+          <div className="responsabilita">RESPONSABILITÀ</div>
+          <div className="community">COMMUNITY</div>
+          <div className="sudinoi">SU DI NOI</div>
+        </div>
+
+        <div className="info" > Un gusto unico come quello di casa. Scopri i nostri caffè Starbucks<sup>®</sup>
+         da poter preparare comodamente a casa tua. <br /> Goditi una tazza di caffè 100% Arabica o una bevanda Starbucks <sup>®</sup> 
+        , quando e come vuoi tu.</div>
+        <div className="bio"></div>
     </div>
   );
 }
