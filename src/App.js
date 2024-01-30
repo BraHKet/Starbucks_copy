68752861e98612ac4ec636d1d06d3fd48bbe3e8f
Lorenzo <lore.mail.gl@gmail.com>
2024-01-30 14:14:35 +0100

    Versione aggiornata v1.0.1

--- src/App.js
+++ src/App.js
@@ -5,21 +5,50 @@ import logo from './img/starb.png';
 function App() {
   return (
     <div className="container">
+
         <div className="header">
           <img src={logo} alt=''/>
           <div className="caffe">CAFFÈ
             <div className="sub_caffe">
               <ul>
-                <li id='caffe1'>Il nostro caffè</li>
-                <li id='caffe2'>Espresso</li>
-                <li id='caffe3'>Preparazione caffè</li>
+                <li className='caffe1'>Il nostro caffè</li>
+                <li className='caffe2'>Espresso</li>
+                <li className='caffe3'>Preparazione caffè</li>
               </ul>
             </div>
           </div>
-          <div className="menu">MENÙ</div>
-          <div className="responsabilita">RESPONSABILITÀ</div>
+          <div className="menu">MENÙ
+            <div className="sub_menu">
+              <ul>
+                <li className='menu1'>Bevande Starbucks</li>
+                <li className='menu2'>Cibo</li>
+              </ul>
+            </div>
+          </div>
+          <div className="responsabilita">RESPONSABILITÀ
+            <div className="sub_resp">
+              <ul>
+                <li className='resp1'>Approvvigionamento di caffè</li>
+                <li className='resp2'>Sostegno agli agricoltori</li>
+                <li className='resp3'>Tè di provenienza etica</li>
+                <li className='resp4'>Approvvigionamento di cacao</li>
+              </ul>
+            </div>
+          </div>
           <div className="community">COMMUNITY</div>
-          <div className="sudinoi">SU DI NOI</div>
+          <div className="sudinoi">SU DI NOI
+            <div className="sub_sudinoi">
+              <ul>
+                <li className='sudinoi1'>Store Location</li>
+                <li className='sudinoi2'>Informazioni aziendali</li>
+                <li className='sudinoi3'>Il nostro patrimonio</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="imgHome">
+          <img src='https://www.starbucks.it/wp-content/uploads/2023/01/Banner_Hero_HP.jpg' alt=''/>
         </div>
 
         <div className="info" > Un gusto unico come quello di casa. Scopri i nostri caffè Starbucks<sup>®</sup>
