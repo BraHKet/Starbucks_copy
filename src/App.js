a81ce15d9feb953fbff982485b97bd5e1a619573
Lorenzo <lore.mail.gl@gmail.com>
2024-02-20 17:53:26 +0100

    Versione v1.0.6 In procinto di modificare l'header per renderlo responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import logo from './img/starb.png';
 
 
 function App() {
+  const [variabile, setVaribile] = useState(false);
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+  
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    
+    if(windowSize<920) {
+      setVaribile(false);   
+    }
+    else {
+      setVaribile(true);
+    }
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [windowSize]);
+
+
+
+
   return (
     <div className="container">
-
-        <div className="header">
+        {variabile === true && <div className="header">
           <img src={logo} alt=''/>
           <div className="caffe">CAFFÈ
             <div className="sub_caffe">
@@ -34,7 +59,7 @@ function App() {
                 <li className='resp4'>Approvvigionamento di cacao</li>
               </ul>
             </div>
-          </div>
+        </div>
           <div className="community">COMMUNITY</div>
           <div className="sudinoi">SU DI NOI
             <div className="sub_sudinoi">
@@ -45,7 +70,16 @@ function App() {
               </ul>
             </div>
           </div>
-        </div>
+        </div>}
+
+        {variabile === false && <div className="header">
+            <a href='www.google.com'>ciao</a>
+          </div>}
+
+
+
+
+
 
         <div className="imgHome">
           <img src='https://www.starbucks.it/wp-content/uploads/2023/01/Banner_Hero_HP.jpg' alt=''/>
@@ -60,24 +94,29 @@ function App() {
 
         <div className="ilnostromenu">
           <h1>IL NOSTRO MENÙ</h1>
-          <div className="separatore">
-        </div>
+          <div className="separatore"></div>
 
         <div className="immaginimenu">
-          <div className="imgmenu1"></div>
-          <div className="imgmenu2"></div>
-          
-          <div className="sott-container">
+          <div className="imgmenu1">
             <div className="imgmenu1-sott">CIBO STARBUCKS</div>
-            <div className="imgmenu2-sott">BEVANDE STARBUCKS</div>
           </div>
-          
+          <div className="imgmenu2">
+          <div className="imgmenu2-sott">BEVANDE STARBUCKS</div>
+          </div>
         </div>
-          
+        </div>
+
+      <div className="inostricaffe">
+        <div className="testo">
+          <h1>I NOSTRI CAFFÉ</h1>
+          <p>Hai trovato il tuo caffè Starbucks <sup>®</sup> preferito da preparare a casa? Perché non conoscere di più il mondo del caffè?
+             Bevi un caffè e goditi i nostri articoli sul viaggio del caffè, la sostenibilità e lo stile di vita.</p>
+          <br/>
+          <a href='www.google.com'>Scopri di più</a>
+
+        </div>
         
-
-
-        </div>
+      </div>
 
     </div>
   );
